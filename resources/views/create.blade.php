55bca83cedfd597b5c8f390f5b7b6693bcf46882
Arry <arianur098@gmail.com>
2019-02-13 21:51:26 -0800

    add content on create.blade.php

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -7,5 +7,15 @@
 
 	<h1>Halaman Create Post</h1>
 
+	<form action="http://localhost/laravel/cobalaravel/public/post/store" method="post">
+		<label for="title">Title</label>
+		<input type="text" name="title" id="title">
+		<br>
+		<label for="body">Body</label>
+		<textarea name="body" id="body" cols="30" rows="10"></textarea>
+		<br>
+		<button type="submit">Submit</button>
+	</form>
+
 </body>
 </html>
