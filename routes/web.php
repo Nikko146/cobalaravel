998cbbade649cf80f2e1d50381cdcd39616a2f7c
Arry <arianur098@gmail.com>
2019-02-13 18:53:31 -0800

    delete number 1 on body from variable array posts on web.php

--- routes/web.php
+++ routes/web.php
@@ -21,9 +21,9 @@ Route::get('/about', function() {
 
 Route::get('/blog', function () {
 	$posts = [
-		['title' => 'post 1', 'body 1' => 'Body post in ID 1'],
-		['title' => 'post 2', 'body 1' => 'Body post in ID 2'],
-		['title' => 'post 3', 'body 1' => 'Body post in ID 3'],
-		['title' => 'post 4', 'body 1' => 'Body post in ID 4'],
+		['title' => 'post 1', 'body' => 'Body post in ID 1'],
+		['title' => 'post 2', 'body' => 'Body post in ID 2'],
+		['title' => 'post 3', 'body' => 'Body post in ID 3'],
+		['title' => 'post 4', 'body' => 'Body post in ID 4'],
 	];
 });
