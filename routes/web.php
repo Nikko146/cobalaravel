b0589985f164e93d5cbba00a4176cb18214a01ad
Arry <arianur098@gmail.com>
2019-02-13 18:49:45 -0800

    add content 4 on variable array posts

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,7 @@ Route::get('/blog', function () {
 	$posts = [
 		['title' => 'post 1', 'body 1' => 'Body post in ID 1'],
 		['title' => 'post 2', 'body 1' => 'Body post in ID 2'],
-		['title' => 'post 3', 'body 1' => 'Body post in ID 3']
+		['title' => 'post 3', 'body 1' => 'Body post in ID 3'],
+		['title' => 'post 4', 'body 1' => 'Body post in ID 4'],
 	];
 });
